5495c67ecb5421d0019543d1ecf3da78d8f1f2dd
Diniz-lucas <lucas.diniz@acessodigital.com.br>
2021-06-14 17:32:45 -0300

    fiz bug release nao retorna base64

--- lib/src/core/abstracts/acesso_bio_liveness.interface.dart
+++
@@ -1,8 +0,0 @@
-import 'package:unico_check/src/core/abstracts/acesso_bio.interface.dart';
-import '../response/error/error_bio.response.dart';
-import '../response/success/liveness_x.response.dart';
-
-abstract class IAcessoBioLiveness extends IAcessoBio {
-  void onSuccessLiveness(LivenessXResponse response);
-  void onErrorLiveness(ErrorBioResponse error);
-}
--- lib/src/core/response/error/error_liveness.response.dart
+++
@@ -1,4 +0,0 @@
-class ErrorLivenessResponse {
-  int? code;
-  String? description;
-}
