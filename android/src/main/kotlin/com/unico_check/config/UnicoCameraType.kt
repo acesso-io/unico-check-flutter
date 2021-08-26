93fa46235c443b2844477aa17c3941fb69a555fc
Diniz-Lucas <Lucas.diniz@acessodigital.com.br>
2021-08-26 17:39:01 -0300

    fix: remove class not used and remove comments

--- android/src/main/kotlin/com/unico_check/config/UnicoCameraType.kt
+++
@@ -1,9 +0,0 @@
-package com.unico_check.config
-
-class UnicoCameraType(
-    autoCapture: Boolean?,
-    smartFrame: Boolean?
-) {
-    var autoCapture: Boolean = true
-    var smartFrame: Boolean = true
-}
