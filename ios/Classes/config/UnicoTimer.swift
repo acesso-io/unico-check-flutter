8744505f005ad24d78d43467cd2350e42204bce0
Diniz-Lucas <Lucas.diniz@acessodigital.com.br>
2021-12-14 10:41:25 -0300

    fix: set constant

--- ios/Classes/config/UnicoTimer.swift
+++ ios/Classes/config/UnicoTimer.swift
@@ -9,12 +9,14 @@ import Foundation
 
 class UnicoTimer {
     
-    private var timeoutSession: Double
-    private var timeoutToFaceInference: Double
+    private let defaultTimeSession = 50.0
+    private let defaultTimeFaceInference = 15.0
+    private let timeoutSession: Double
+    private let timeoutToFaceInference: Double
     
     init(argument: Dictionary<String, Any>) {
-        self.timeoutSession = argument[MethodConstansts.setTimeoutSession] as? Double ?? 50.0
-        self.timeoutToFaceInference = argument[MethodConstansts.setTimeoutToFaceInference] as? Double ?? 15.0
+        self.timeoutSession = argument[MethodConstansts.setTimeoutSession] as? Double ?? defaultTimeSession
+        self.timeoutToFaceInference = argument[MethodConstansts.setTimeoutToFaceInference] as? Double ?? defaultTimeFaceInference
     }
 
     func getTimeoutSession() -> Double{
