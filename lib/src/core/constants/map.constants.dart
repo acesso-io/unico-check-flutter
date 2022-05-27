47bb370b9d4fa2acca24d63793f423d2cfd3e496
Lucas diniz silva <74009931+Diniz-Lucas@users.noreply.github.com>
2022-05-27 16:39:46 -0300

    feat: remove old estructure (#34)
    * feat: remove old classes test ins not to clean arch
    
    * feat: delete old estructure
    
    * feat: test example
    
    * no message
    
    * feat: add test
    
    Co-authored-by: lucas-diniz <lukas-diniz@hotmail.com>

--- lib/src/core/constants/map.constants.dart
+++
@@ -1,7 +0,0 @@
-class MapConstants {
-  static const flutterStatus = 'flutterstatus';
-  static const documentType = 'DOCUMENT_TYPE';
-  static const result = 'result';
-  static const disableAutoCapture = 'disableAutoCapture';
-  static const disableSmartFrame = 'disableSmartFrame';
-}
