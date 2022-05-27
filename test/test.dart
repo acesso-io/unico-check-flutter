056f4916e59e538b2e8437ee5636b2fc38524e7d
lucas-diniz <lukas-diniz@hotmail.com>
2022-05-27 16:36:07 -0300

    no message

--- test/test.dart
+++ test/test.dart
@@ -1,17 +1,11 @@
-import 'package:flutter/services.dart';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:unico_check/src/core/constants/methods_channels.constants.dart';
-import 'package:unico_check/src/core/constants/response_contants.dart';
-import 'package:unico_check/src/functions/camera.functions.dart';
-import 'package:unico_check/unico_check.dart';
+
 
 bool returnedInInterface = false;
 
 void main() {
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  test('create_unico_check', () async {
+  test('create_unico_check', () {
 
   });
 }
